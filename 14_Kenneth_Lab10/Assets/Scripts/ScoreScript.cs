000018799912c735e15b0d75b2a710b477da123e
A-Cute-Loli <146807353+A-Cute-Loli@users.noreply.github.com>
2023-10-31 14:26:21 +0800

    Added Unity Project

---
+++ 14_Kenneth_Lab10/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    [SerializeField] private Text ScoreText;
+    private int Score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            Destroy(other.gameObject);
+            Score++;
+            ScoreText.GetComponent<Text>().text = "Score: " + Score;
+        }
+    }
+
+}
